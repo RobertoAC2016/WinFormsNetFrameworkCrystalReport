dcecf27ce3769e9b0cefef8a80b90126dc75e74d
Roberto Avila <ravila@dealergeek.com>
2023-11-30 12:10:49 -0600

    Primer commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,66 @@
+﻿namespace WinFormsNetFrameworkCrystalRep
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.crystalReportViewer1 = new CrystalDecisions.Windows.Forms.CrystalReportViewer();
+            this.crv11 = new WinFormsNetFrameworkCrystalRep.crv1();
+            this.SuspendLayout();
+            // 
+            // crystalReportViewer1
+            // 
+            this.crystalReportViewer1.ActiveViewIndex = 0;
+            this.crystalReportViewer1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.crystalReportViewer1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.crystalReportViewer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.crystalReportViewer1.Location = new System.Drawing.Point(0, 0);
+            this.crystalReportViewer1.Name = "crystalReportViewer1";
+            this.crystalReportViewer1.ReportSource = this.crv11;
+            this.crystalReportViewer1.Size = new System.Drawing.Size(1107, 705);
+            this.crystalReportViewer1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1107, 705);
+            this.Controls.Add(this.crystalReportViewer1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private CrystalDecisions.Windows.Forms.CrystalReportViewer crystalReportViewer1;
+        private crv1 crv11;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,76 @@
+﻿using CrystalDecisions.CrystalReports.Engine;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsNetFrameworkCrystalRep
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //Aqui solo traigo los datos para mostrarlos
+        public DataTable Data()
+        {
+            DataTable dt = new DataTable();
+            dt.Columns.Add("Id", typeof(int));
+            dt.Columns.Add("Name", typeof(string));
+            dt.Columns.Add("Description", typeof(string));
+            foreach (var item in Products.GetItems())
+            {
+                dt.Rows.Add(item.Id, item.Name, item.Description);
+            }
+            return dt;
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Aqui es donde se usaran los datos previamente traidos desde data para mostrarlos en el CRV
+            ReportDocument rpt = new ReportDocument();
+            rpt.Load(@"D:\RAC\NET\WinFormsNetFrameworkCrystalRep\crv1.rpt");
+            rpt.SetDataSource(Data());
+            crystalReportViewer1.ReportSource = rpt;
+        }
+    }
+    //estos son datos de tus fuente de datos, ya sea el resultado de un querie o de tu datagridview, asi que aqui no importan
+    //Solo los utilizo para mostrar datos en el Cristal Report
+    public static class Products
+    {
+        public static List<Item> GetItems()
+        {
+            return new List<Item> {
+                new Item
+                {
+                    Id = 1,
+                    Name = "Pera",
+                    Description = "Fruta dulce"
+                },
+                new Item
+                {
+                    Id = 2,
+                    Name = "Cebolla",
+                    Description = "Verdura"
+                },
+                new Item
+                {
+                    Id = 3,
+                    Name = "Bistek",
+                    Description = "Carne"
+                },
+            };
+        }
+    }
+    public class Item
+    {
+        public int Id { get; set; }
+        public String Name { get; set; }
+        public String Description { get; set; }
+    }
+}
